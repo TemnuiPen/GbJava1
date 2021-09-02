3a9f2b34b946d9b01716b5c43b0f2b4c213f46ea
Leha <maltsev0017@gmail.com>
2021-09-02 13:39:32 +0300

    Checked and finished third and fourth tasks for homework 3

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -4,6 +4,8 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         firstTask();
         secondTask();
+        thirdTask();
+        fourthTask();
     }
 
     private static void fifthTask() {
@@ -11,10 +13,42 @@ public class HomeWorkApp {
     }
 
     private static void fourthTask() {
+        int [][] arr = new int [4][4];
+        int arrLength = arr.length;
+        int counter = arrLength - 1;
+        for (int i = 0; i < arrLength; i++) {
+            for (int j = 0; j < arrLength; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                if(counter == j) {
+                    arr[i][j] = 1;
+                    counter--;
+                }
+            }
+        }
 
+        System.out.println("");
+        System.out.println("Here fourth task begins");
+        for (int i = 0; i < arrLength; i++) {
+            for (int j = 0; j < arrLength; j++) {
+                System.out.print(" " + arr[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 
     private static void thirdTask() {
+        int [] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int arrLength = arr.length;
+        for (int i = 0; i < arrLength; i++) {
+            if(arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+        }
+        System.out.println("");
+        System.out.println("Here third task begins");
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void secondTask() {
@@ -29,7 +63,7 @@ public class HomeWorkApp {
     }
 
     private static void firstTask() {
-        int [] a = {1,0,0,1,0,1,0,1};
+        int [] a = {1, 0, 0, 1, 0, 1, 0, 1};
         for (int i = 0; i < a.length; i++) {
             if (a[i] == 0) {
                 a[i] = 1;
