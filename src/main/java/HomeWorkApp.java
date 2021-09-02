9778101cbccc36c0722d47e0c6171f40ea742ef5
Leha <maltsev0017@gmail.com>
2021-09-02 12:36:01 +0300

    The first task for homework 3

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,53 +1,30 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
-        firstTask(2,45);
-        secondTask(-12);
-        thirdTask(10);
-        fourthTask("Hello mazafaka", 8);
-        fifthTask(2000);
+
     }
 
-    private static boolean fifthTask(int year) {
-        if(year % 4 == 0 && year % 100 != 0) {
-            System.out.println("Год високосный");
-            return true;
-        }
-        else if (year % 400 == 0) {
-            System.out.println("Год високосный");
-            return true;
-        }
-        else {
-            System.out.println("Год не високосный");
-            return false;
-        }
+    private static void fifthTask() {
+
     }
 
-    private static void fourthTask(String str, int number) {
-        int i = 0;
-        while (i < number) {
-            System.out.println(str);
-            i++;
-        }
+    private static void fourthTask() {
+
     }
 
-    private static boolean thirdTask(int a) {
-        if (a >= 0) {
-            return false;
-        }
-        else return true;
+    private static void thirdTask() {
     }
 
-    private static void secondTask(int a) {
-        if (a >= 0) {
-            System.out.println("Положительное число");
-        }
-        else System.out.println("Отрицательное число");
+    private static void secondTask() {
+
     }
 
-    private static boolean firstTask(int a, int b) {
-        if (a + b > 10 && a + b < 20) {
-            return true;
+    private static void firstTask() {
+        int [] a = {1,0,0,1,0,1,0,1};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 0) {
+                a[i] = 1;
+            }
+            else a[i] = 0;
         }
-        else return false;
     }
 }
