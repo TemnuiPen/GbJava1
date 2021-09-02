6473a0cad309243c264fa5cff49f68ecb3f81851
Leha <maltsev0017@gmail.com>
2021-09-02 12:46:56 +0300

    Checked first task and finished second task task for homework 3

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        firstTask();
+        secondTask();
     }
 
     private static void fifthTask() {
@@ -15,7 +18,14 @@ public class HomeWorkApp {
     }
 
     private static void secondTask() {
-
+        int arrLength = 100;
+        int [] arr = new int[arrLength];
+        for (int i = 0; i < arrLength; i++) {
+            arr[i] = i + 1;
+        }
+        System.out.println("");
+        System.out.println("Here second task begins");
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void firstTask() {
@@ -26,5 +36,7 @@ public class HomeWorkApp {
             }
             else a[i] = 0;
         }
+        System.out.println("Here first task begins");
+        System.out.println(Arrays.toString(a));
     }
 }
