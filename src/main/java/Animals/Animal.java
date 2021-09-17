35140d9849f0889dc5e1e580e2f04588ff88ea0e
Leha <maltsev0017@gmail.com>
2021-09-17 15:38:34 +0300

    First commit

--- src/main/java/Animals/Animal.java
+++
@@ -1,7 +0,0 @@
-package Animals;
-
-public abstract class Animal {
-    public abstract void run(int distance, String name);
-
-    public abstract void swim(int distance, String name);
-}
--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,28 +1,9 @@
-import Animals.Cat;
-import Animals.Dog;
 
 import java.util.Arrays;
 
 public class MainApp {
-    private static int counter = 0;
-    private static String[] animalNames = new String[5];
 
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik");
-        cat.swim(120, cat.getName());
-        cat.run(200, cat.getName());
-        animalNames[0] = cat.getName();
-        Dog dog = new Dog("Bobik");
-        dog.swim(120, dog.getName());
-        dog.run(220, dog.getName());
-        animalNames[1] = dog.getName();
-        for (int i = 0; i < animalNames.length; i++) {
-            if(animalNames[i] != null) {
-                counter++;
-            }
-        }
-        System.out.println("Общее количество животных  - " + counter);
-
 
     }
 
